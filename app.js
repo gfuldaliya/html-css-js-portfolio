21637403db4b4ceed8aadbdbc8a2999dfb019c60
gfuldaliya <gfuldaliya17@gmail.com>
2024-08-11 14:46:12 +0530

    added some hover effects and typed effects

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Web Designer"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
